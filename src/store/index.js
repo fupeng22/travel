a9d184d1abb6f8830ee5d743473f6189ffb35203
fupeng <509243490@qq.com>
2020-06-25 19:42:52 +0800

    commit

--- src/store/index.js
+++ src/store/index.js
@@ -8,10 +8,5 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state:state,
-    mutations,
-    getters:{
-        doubleCity(state){
-            return state.city+" "+state.city
-        }
-    }
+    mutations
 })
